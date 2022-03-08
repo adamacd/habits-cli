0335f77e7f7a03646c2323dc70343590fb21235c
adamacd <76454079+adamacd@users.noreply.github.com>
2022-03-07 22:47:25 -0500

    added get data

--- database.py
+++ database.py
@@ -19,11 +19,23 @@ id = 4
 periodicity = 'weekly'
 description = 'new format testin'
 date = '2022-03-07, 15:13'
+'''
 cur.execute (
     "INSERT INTO habitsTable VALUES (?,?,?,?, NULL, NULL, NULL)", 
     (id, periodicity, description, date)
 )
 print("successfully added")
+'''
+
+# get data from db
+cur.execute(
+    "SELECT * FROM habitsTable WHERE habit_id == ?",
+    [id] #note, gotta be list, or else will be an error
+    )
+
+print(cur.fetchone())
+
+
 
 # submit changes
 conn.commit()
