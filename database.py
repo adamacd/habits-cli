2af39a6ed595b2f37443ad54cfca4885984561c9
adamacd <76454079+adamacd@users.noreply.github.com>
2022-03-10 02:28:39 -0500

    Add files via upload

--- database.py
+++ database.py
@@ -1,4 +1,16 @@
 import sqlite3
+
+
+sql_tablE_columns = ["habit_id", "periodicity/TEXT", "description/TEXT",
+                     "datetime_created/TEXT", "datetime_last_completed/TEXT nullable",
+                     "streak/INTEGER nullable", "active/TEXT nullable"
+                     ]
+def columnNames(list):
+    for element in list:
+        print(element + "\n")
+columnNames(sql_tablE_columns)
+
+
 ''' SQL TABLE COLUMNS
 habit_id
 periodicity/TEXT
@@ -39,13 +51,23 @@ print(cur.fetchone())
 
 # Formatting results for analysis: show current, 
 # show all, all daily, all weekly,
+'''
 cur.execute(
     "SELECT * FROM habitsTable"
 )
 items = cur.fetchall()
 for element in items:
     print(str(element[0]) + " " + element[1] + "   " + element[2]) #ect.
+'''
 
+cur.execute("""
+            UPDATE habitsTable 
+            SET datetime_last_completed = '2022-03-08, 11:42', 
+            streak = 1, 
+            active = 'Yes'
+            WHERE habit_id == 3
+            """
+            )
 
 # submit changes
 conn.commit()
